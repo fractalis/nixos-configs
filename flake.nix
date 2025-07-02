6b442f4515d0f47e1a0ac6da5eeefd39759df757
Scott Rallya <185441+fractalis@users.noreply.github.com>
2025-07-02 16:30:17 -0400

    Flake Updates

--- flake.nix
+++ flake.nix
@@ -4,19 +4,61 @@
   inputs = {
     # While more risky, going to opt for unstable for bleeding edge and
     # the latest and greatest.
-    nixpkgs.follows = "nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Support other nixpkgs branches.
-    nixos-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # ---
+    # NUR (Nix User Repository)
+    nur.url = "github:nix-community/NUR";
 
-    # Local
-    infinitas-core.url = "path:./core";
-    # ---
+    # Snowfall
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # NixOS Hardware
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    # Managing Secrets
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    impermanence.url = "github:nix-community/impermanence";
+
+    nixgl.url = "github:nix-community/nixGL";
+    nix-index-database.url = "github:nix-community/nix-index-database";
+
+    # Disko
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    # nixos-anywhere
+    nixos-anywhere.url = "github:nix-community/nixos-anywhere";
+    nixos-anywhere.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-anywhere.inputs.disko.follows = "disko";
+
+    # Generate NixOS systems to various formats
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Comma
+    comma.url = "github:nix-community/comma";
+    comma.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Hyperland
+    hyprland-contrib.url = "github:hyprwm/contrib";
+    hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
+
+    hyprcursor.url = "github:hyprwm/Hyprcursor";
+    hyprcursor.inputs.nixpkgs.follows = "nixpkgs";
+
+    pyprland.url = "github:hyprland-community/pyprland";
+    pyprland.inputs.nixpkgs.follows = "nixpkgs";
 
     # Agenix
     agenix.url = "github:ryantm/agenix";
@@ -37,10 +79,6 @@
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Disko
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-
     # Fenix
     fenix.url = "github:nix-community/fenix";
     fenix.inputs.nixpkgs.follows = "nixpkgs";
@@ -53,20 +91,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-    # Home Manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Hyperland
-    hyprland-contrib.url = "github:hyprwm/contrib";
-    hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
-
-    hyprcursor.url = "github:hyprwm/Hyprcursor";
-    hyprcursor.inputs.nixpkgs.follows = "nixpkgs";
-
-    pyprland.url = "github:hyprland-community/pyprland";
-    pyprland.inputs.nixpkgs.follows = "nixpkgs";
-
     # Microvm
     microvm.url = "github:astro/microvm.nix";
     microvm.inputs.nixpkgs.follows = "nixpkgs";
@@ -75,34 +99,9 @@
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
-    # nixos-anywhere
-    nixos-anywhere.url = "github:nix-community/nixos-anywhere";
-    nixos-anywhere.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-anywhere.inputs.disko.follows = "disko";
-
-    # nixos-hardware
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-
-    # Generate NixOS systems to various formats
-    nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Managing Secrets
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Impermanence
-    impermanence.url = "github:nix-community/impermanence";
-
     nix-fast-build.url = "github:Mic92/nix-fast-build";
     nix-fast-build.inputs.flake-parts.follows = "flake-parts";
     nix-fast-build.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
-    nur-packages.url = "github:nix-community/NUR";
-    nur-packages.inputs.nixpkgs.follows = "nixpkgs";
 
     git-hooks.url = "github:cachix/git-hooks.nix";
     git-hooks.inputs.nixpkgs.follows = "nixpkgs";
