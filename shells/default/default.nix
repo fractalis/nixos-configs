c0cb57fd0f93e8804c19376f2edc3d6d05c58212
Scott Rallya <185441+fractalis@users.noreply.github.com>
2025-07-04 19:15:03 -0400

    Initial default shell

---
+++ shells/default/default.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+pkgs.mkShell {
+  NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+
+  packages = with pkgs; [
+    nh
+    inputs.nixos-anywhere.packages.${pkgs.system}.nixos-anywhere
+    python312Packages.mkdocs-material
+
+    deploy-rs
+
+    statix
+    deadnix
+
+    alejandra
+    home-manager
+    git
+    sops-nix
+    ssh-to-age
+    gnupg
+    age
+  ];
+}
